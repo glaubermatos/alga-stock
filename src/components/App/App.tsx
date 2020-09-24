05717b615abb473e60eec8bffd5c61a1aef97f6d
glaubermatos <glaub.oliveira@hotmail.com>
2020-09-24 00:46:02 -0300

    deleting component TestComponet

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import './App.css';
-import TextComponent from '../TestComponent';
-
-const user = {
-  name: 'Glauber',
-  age: 32,
-  email: 'glaub.oliveira@hotmail.com'
-}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Bem vindo!
-        </p>
-        <TextComponent user={user} />
-      </header>
+      App
     </div>
   );
 }
