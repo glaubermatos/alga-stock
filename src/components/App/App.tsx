ac2243566234d4b5d072878886f9a8dfa3ca4670
glaubermatos <glaub.oliveira@hotmail.com>
2020-09-24 00:59:05 -0300

    reset css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      App
+      <h1>App</h1> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, eaque qui numquam cum amet ex soluta adipisci corrupti nisi excepturi voluptas debitis optio, tempore quod quidem eum, consequatur a hic!</p>
     </div>
   );
 }
