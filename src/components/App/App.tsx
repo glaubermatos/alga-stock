19b3a6393bffcc84ef6e7427287faacea3a4fec2
glaubermatos <glaub.oliveira@hotmail.com>
2020-09-24 01:29:58 -0300

    add header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from '../Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1> 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, eaque qui numquam cum amet ex soluta adipisci corrupti nisi excepturi voluptas debitis optio, tempore quod quidem eum, consequatur a hic!</p>
+      <Header title='AlgaStock'/>  
     </div>
   );
 }
