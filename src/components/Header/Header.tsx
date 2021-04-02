9e6d9107ea04b5151a5d1c3792f25bda3662d128
glaubermatos <glaub.oliveira@hotmail.com>
2021-04-02 02:28:04 -0300

    Header component connected with state redux

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { RootState } from '../../redux'
+
 import './styles.css'
 
 declare interface HeaderProps {
-    title: string;
+    title: string
+    stockProducts: number
 }
 
-const Header: React.FC<HeaderProps> = ({ title }) => {
+const Header: React.FC<HeaderProps> = (props) => {
     return (
         <header className='AppHeader'>
-            <h1>{title}</h1>
+            <h1>{props.title}</h1>
+            <p>
+                Stock <strong>{props.stockProducts}</strong>
+            </p>
         </header>
     )
 }
 
-export default Header
+const mapStateToProps = (state: RootState) => ({
+    stockProducts: state.products.length
+})
+
+export default connect(mapStateToProps)(Header)
